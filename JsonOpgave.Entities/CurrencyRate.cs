4c548eddcbc89bf30ab8d8e2fc714c9122a1d3bd
Rasmus <33279132+reesmees@users.noreply.github.com>
2018-08-20 11:01:48 +0200

    Reworked CurrencyRate class

--- JsonOpgave.Entities/CurrencyRate.cs
+++ JsonOpgave.Entities/CurrencyRate.cs
@@ -8,11 +8,48 @@ namespace JsonOpgave.Entities
 {
     public class CurrencyRate
     {
-        public string Disclaimer { get; set; }
-        public string License { get; set; }
-        public long Timestamp { get; set; }
-        public string Base { get; set; }
-        public Dictionary<string, decimal> Rates { get; set; }
+        private string disclaimer;
+        private string license;
+        private long timestamp;
+        private string baseCurrency;
+        private Dictionary<string, decimal> rates;
+
+        public Dictionary<string, decimal> Rates
+        {
+            get { return rates; }
+            set { rates = value; }
+        }
+
+        public string Base
+        {
+            get { return baseCurrency; }
+            set {
+                if (value == "USD")
+                    baseCurrency = value;
+                else
+                    throw new ArgumentException("Base currency must be USD");
+            }
+        }
+
+
+        public long Timestamp
+        {
+            get { return timestamp; }
+            set { timestamp = value; }
+        }
+
+        public string License
+        {
+            get { return license; }
+            set { license = value; }
+        }
+
+        public string Disclaimer
+        {
+            get { return disclaimer; }
+            set { disclaimer = value; }
+        }
+
 
 
     }
