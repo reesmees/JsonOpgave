4b0c429047db7a3497f996ed22e8fc6b2f396fad
Rasmus <33279132+reesmees@users.noreply.github.com>
2018-08-20 11:37:00 +0200

    Added summaries to the CurrencyRate and WebService classes

--- JsonOpgave.Entities/CurrencyRate.cs
+++ JsonOpgave.Entities/CurrencyRate.cs
@@ -6,44 +6,59 @@ using System.Threading.Tasks;
 
 namespace JsonOpgave.Entities
 {
+    /// <summary>
+    /// Represents the exchange rates of different currencies in relation to USD
+    /// </summary>
     public class CurrencyRate
     {
         private string disclaimer;
         private string license;
         private long timestamp;
-        private string baseCurrency;
+        private string _base;
         private Dictionary<string, decimal> rates;
 
+        /// <summary>
+        /// The exchange rates of different currencies in relation to USD
+        /// </summary>
         public Dictionary<string, decimal> Rates
         {
             get { return rates; }
             set { rates = value; }
         }
-
+        /// <summary>
+        /// The base currency upon which the exchange rates are based.
+        /// Must be set to USD.
+        /// </summary>
         public string Base
         {
-            get { return baseCurrency; }
+            get { return _base; }
             set {
                 if (value == "USD")
-                    baseCurrency = value;
+                    _base = value;
                 else
                     throw new ArgumentException("Base currency must be USD");
             }
         }
 
-
+        /// <summary>
+        /// Representation of when the data was obtained
+        /// </summary>
         public long Timestamp
         {
             get { return timestamp; }
             set { timestamp = value; }
         }
-
+        /// <summary>
+        /// License for the openexchangerate website
+        /// </summary>
         public string License
         {
             get { return license; }
             set { license = value; }
         }
-
+        /// <summary>
+        /// Disclaimer for the openexchangerates website
+        /// </summary>
         public string Disclaimer
         {
             get { return disclaimer; }
--- JsonOpgave.Service/WebService.cs
+++ JsonOpgave.Service/WebService.cs
@@ -9,21 +9,33 @@ using System.Net;
 
 namespace JsonOpgave.Service
 {
+    /// <summary>
+    /// Used to deserialize a JSON object to the CurrencyRate class
+    /// </summary>
     public class WebService
     {
         private string url;
 
+        /// <summary>
+        /// Constructor for the WebService class
+        /// </summary>
+        /// <param name="url">The URL of the JSON object to be deserialized</param>
         public WebService(string url)
         {
             Url = url;
         }
-
+        /// <summary>
+        /// The URL of the JSON object to be deserialized
+        /// </summary>
         public string Url
         {
             get { return url; }
             set { url = value; }
         }
-
+        /// <summary>
+        /// Deserializes the JSON object retrieved from the URL into an object of the CurrencyRate type
+        /// </summary>
+        /// <returns>A CurrencyRate object containing the information of the JSON object</returns>
         public CurrencyRate GetCurrencyRate()
         {
             using(WebClient client = new WebClient()) {
