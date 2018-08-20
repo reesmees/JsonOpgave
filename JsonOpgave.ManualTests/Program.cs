10278028cd47ef7e06e3fae3197aca5e8272fefb
Rasmus <33279132+reesmees@users.noreply.github.com>
2018-08-20 10:57:55 +0200

    Created WebService class, set up basic test

--- JsonOpgave.ManualTests/Program.cs
+++ JsonOpgave.ManualTests/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using JsonOpgave.Entities;
+using JsonOpgave.Service;
 
 namespace JsonOpgave.ManualTests
 {
@@ -10,6 +12,12 @@ namespace JsonOpgave.ManualTests
     {
         static void Main(string[] args)
         {
+            WebService webService = new WebService(@"https://openexchangerates.org/api/latest.json?app_id=5694629fba9d46028ee433bc1b63f11f");
+
+            CurrencyRate currencyRate = webService.GetCurrencyRate();
+
+            Console.WriteLine(currencyRate.Rates["DKK"]);
+            Console.Read();
         }
     }
 }
---
+++ JsonOpgave.Service/WebService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using JsonOpgave.Entities;
+using System.Net;
+
+namespace JsonOpgave.Service
+{
+    public class WebService
+    {
+        private string url;
+
+        public WebService(string url)
+        {
+            Url = url;
+        }
+
+        public string Url
+        {
+            get { return url; }
+            set { url = value; }
+        }
+
+        public CurrencyRate GetCurrencyRate()
+        {
+            using(WebClient client = new WebClient()) {
+                string jsonString = client.DownloadString($@"{url}");
+
+                CurrencyRate currencyRate = JsonConvert.DeserializeObject<CurrencyRate>(jsonString);
+
+                return currencyRate;
+            }
+        }
+    }
+}
