f3917895effc3b04b561c4ccc8ef81e6c61f3dc3
Rasmus <33279132+reesmees@users.noreply.github.com>
2018-08-20 10:45:23 +0200

    Added basic project structure, created CurrencyRate class

---
+++ JsonOpgave.Entities/CurrencyRate.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonOpgave.Entities
+{
+    public class CurrencyRate
+    {
+        public string Disclaimer { get; set; }
+        public string License { get; set; }
+        public long Timestamp { get; set; }
+        public string Base { get; set; }
+        public Dictionary<string, decimal> Rates { get; set; }
+
+
+    }
+}
